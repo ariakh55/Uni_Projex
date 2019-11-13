e35f1695b014c7bfc516b8bbffdbcfe396df8904
Aria <khoshnoodaria@gmail.com>
2019-11-13 21:36:59 +0330

    Simple program showing the Pascal triangle

--- C++/PascalTriangle.cpp
+++ C++/PascalTriangle.cpp
@@ -2,13 +2,27 @@
 
 using namespace std;
 
+//using the Binominal Coeff formula we can get the numerical coefficient
 int binominalCoeff(int n, int k){
     int res = 1;
+    if(k > n - k)
+        k = n - k;
+    for(int i = 0; i < k; i++){
+        res *= (n - i);
+        res /= (i + 1);
+    }
     return res;
 
 }
 
 int main(){
-
+    int len; 
+    cout << "Enter the Length: ";
+    cin >> len; //Determining the length
+    for(int line = 0; line <= len; line++){
+        for(int i = 0; i <= line; i++)
+            cout<<binominalCoeff(line , i)<<" ";
+        cout<<endl;
+    }
     return 0;
 }
