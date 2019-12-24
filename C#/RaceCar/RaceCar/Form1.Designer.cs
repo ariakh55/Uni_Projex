af910032b24f982f746314baa583f3bd507077fd
Aria <khoshnoodaria@gmail.com>
2019-12-24 14:35:10 +0330

    C# exercise

---
+++ C#/RaceCar/RaceCar/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Enemy = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(340, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 120);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(340, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 120);
+            this.label2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(340, 350);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 120);
+            this.label3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(178, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 120);
+            this.label4.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(40, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 470);
+            this.label5.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(680, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 470);
+            this.label6.TabIndex = 3;
+            // 
+            // Enemy
+            // 
+            this.Enemy.BackColor = System.Drawing.Color.Red;
+            this.Enemy.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Enemy.Location = new System.Drawing.Point(80, -100);
+            this.Enemy.Name = "Enemy";
+            this.Enemy.Size = new System.Drawing.Size(80, 120);
+            this.Enemy.TabIndex = 4;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(700, 10);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Score: ";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(747, 10);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(45, 13);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "lblScore";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 462);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Enemy);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Enemy;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ C#/RaceCar/RaceCar/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool isCrashed = false;
+        Random rnd = new Random();
+        Timer timer_carL = new Timer();
+        Timer timer_carR = new Timer();
+        Timer timer_score = new Timer();
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer_score.Enabled = true;
+            lblScore.Text = "0";
+            timer_score.Interval = 850;
+            timer_score.Tick += delegate
+            {
+                score++;
+                lblScore.Text = score.ToString();
+            };
+            timer_carL.Interval = 5;
+            timer_carL.Tick += delegate
+            {
+                label4.Left -= 5;
+                
+            };
+            timer_carR.Interval = 5;
+            timer_carR.Tick += delegate
+            {
+                label4.Left += 5;
+                
+            };
+            timer1.Interval = 1;
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            label1.Top += 5;
+            label2.Top += 5;
+            label3.Top += 5;
+            if (label3.Top >= this.Height)
+                label3.Top = -10;
+            if (label2.Top >= this.Height)
+                label2.Top = -10;
+            if (label1.Top >= this.Height)
+                label1.Top = -10;
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    if (!isCrashed)
+                    {
+                        timer_carL.Start();
+                        if (label4.Left <= label5.Right)
+                        {
+                            isCrashed = true;
+                            timer_carR.Stop();
+                            timer1.Stop();
+                            timer2.Stop();
+                            timer_score.Stop();
+                        }
+                    }
+                    else
+                        return;
+                    break;
+                case Keys.Right:
+                    if (!isCrashed)
+                    {
+                        timer_carR.Start();
+                        if (label4.Right >= label6.Left)
+                        {
+                            isCrashed = true;
+                            timer_carL.Stop();
+                            timer1.Stop();
+                            timer2.Stop();
+                            timer_score.Stop();
+                        }
+                    }
+                    else
+                        return;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    timer_carL.Stop();
+                    break;
+                case Keys.Right:
+                    timer_carR.Stop();
+                    break;
+            }
+        }
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Enemy.Top += 5;
+            if (Enemy.Top >= this.Height)
+            {
+                Enemy.Left = rnd.Next(50, 550);
+                Enemy.Top = -100;
+            }
+        }
+    }
+}
